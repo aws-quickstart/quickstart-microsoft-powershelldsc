f62f9d886720aaaf8c3a74ece3d4b35851305008
Santiago Cardenas <sancard@amazon.com>
2016-07-13 14:03:02 -0700

    Added missing script

---
+++ scripts/configs/DC1-2.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [string]$DomainNetBiosName,
+    [string]$AdminPassword
+)
+
+Remove-Item $env:systemRoot/system32/configuration/pending.mof -force
+Get-Process *wmi* | stop-process -force
+Restart-Service winrm -force
+
+$Pass = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential -ArgumentList "$DomainNetBiosName\administrator", $Pass
+
+Start-DscConfiguration -Path C:\windows\system32\dc1config -Wait -Verbose -Credential $Credential
