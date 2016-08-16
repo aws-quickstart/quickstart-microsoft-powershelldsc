ba094f5497727f57a8aaa9291646424320330a95
Santiago Cardenas <sancard@amazon.com>
2016-08-16 10:31:59 -0700

    Get Pull Server URL from key value. Removed UseBasicParsing as it's not part of Invoke-RestMethod

--- scripts/Invoke-PerformRequiredConfigurationChecks.ps1
+++ scripts/Invoke-PerformRequiredConfigurationChecks.ps1
@@ -5,9 +5,16 @@ try {
     $ErrorActionPreference = "Stop"
 
     # Check to ensure that Pull Server intances behind ELB are ready
-    $PullServerUrl = $(Get-DscLocalConfigurationManager).DownloadManagerCustomData.ServerUrl
+    foreach ($item in $(Get-DscLocalConfigurationManager).DownloadManagerCustomData) {
+        if ($item.key -eq 'ServerUrl') {
+            $PullServerUrl = $item.Value
+        }
+    }
+    if (-not $PullServerUrl) {
+        throw "Unable to retrieve a Pull Server URL from Get-DscLocalConfigurationManager"
+    }
     $tries = 20
-    while (-not $(try{Invoke-RestMethod $PullServerUrl -UseBasicParsing} catch{}) -and $tries -ge 1) {
+    while (-not $(try{Invoke-RestMethod $PullServerUrl} catch{}) -and $tries -ge 1) {
         $tries--
         Write-Verbose "$PullServerUrl not ready. Sleeping for 30 seconds..."
         Start-Sleep -Seconds 30
