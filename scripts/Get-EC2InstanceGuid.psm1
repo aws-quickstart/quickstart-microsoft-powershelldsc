13e19fd1f1312bf1e2c2499ce3109211836cab0f
Santiago Cardenas <sancard@amazon.com>
2016-11-02 13:05:08 -0700

    Update template URL

--- scripts/Get-EC2InstanceGuid.psm1
+++ scripts/Get-EC2InstanceGuid.psm1
@@ -7,12 +7,12 @@
     try {
         Write-Verbose "Downloading CFN Template"
         $wc = new-object System.Net.WebClient
-        $template = $wc.DownloadString('https://s3.amazonaws.com/quickstart-reference/microsoft/powershelldsc/latest/templates/Template_1_DSC.template') | ConvertFrom-Json
-    
+        $template = $wc.DownloadString('https://s3.amazonaws.com/quickstart-reference/microsoft/powershelldsc/latest/templates/dsc-pull.template') | ConvertFrom-Json
+
         Write-Verbose "Retrieving instance guid"
         $guid = $template.Resources.$InstanceName.Properties.Tags.where{$_.key -eq 'guid'}.value
 
-        Write-Output $guid    
+        Write-Output $guid
     }
     catch {
         $_ | Write-AWSQuickStartException
