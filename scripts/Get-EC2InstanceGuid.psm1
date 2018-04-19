3a53af017e62c3a0bcffb24f09557aa03ac9a8de
Santiago Cardenas <sancard@amazon.com>
2018-04-18 19:07:27 -0700

    Update default values for bucket name and key prefix. Add S3 portability changes.

--- scripts/Get-EC2InstanceGuid.psm1
+++ scripts/Get-EC2InstanceGuid.psm1
@@ -7,7 +7,7 @@
     try {
         Write-Verbose "Downloading CFN Template"
         $wc = new-object System.Net.WebClient
-        $template = $wc.DownloadString('https://s3.amazonaws.com/quickstart-reference/microsoft/powershelldsc/latest/templates/dsc-pull.template') | ConvertFrom-Json
+        $template = $wc.DownloadString('https://aws-quickstart.s3.amazonaws.com/quickstart-microsoft-powershelldsc/templates/dsc-pull.template') | ConvertFrom-Json
 
         Write-Verbose "Retrieving instance guid"
         $guid = $template.Resources.$InstanceName.Properties.Tags.where{$_.key -eq 'guid'}.value
